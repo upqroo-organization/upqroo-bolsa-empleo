75588c70fb05a9347d73a4499b24d359749138f7
Edgar Pech Garcia <71201852+Nfsedg@users.noreply.github.com>
2025-07-27 12:47:45 -0500

    Add prisma command on deploy process

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
+  },
   async rewrites() {
     return [
       {
